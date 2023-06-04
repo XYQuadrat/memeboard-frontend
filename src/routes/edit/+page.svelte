69b8f6ff4d6dda879c848117fb629e1681a5dbf9
xyquadrat <mail@xyquadrat.ch>
2023-06-04 17:45:03 +0200

    Update tag and edit to match UI update

--- src/routes/edit/+page.svelte
+++ src/routes/edit/+page.svelte
@@ -7,6 +7,7 @@
 		type InfiniteLoadingProps
 	} from 'svelte-infinite-loading';
 	import Svelecte from 'svelecte/src/Svelecte.svelte';
+	import { ChevronUp, Link } from 'lucide-svelte';
 
 	interface Meme {
 		id: number;
@@ -92,9 +93,9 @@
 	<title>D-INFK Memeboard</title>
 </svelte:head>
 
-<main id="content" class="dark:bg-zinc-800 dark:text-gray-100 px-4">
+<main id="content" class="px-4">
 	<h1 class="text-center text-4xl py-6 font-bold">D-INFK Discord Memeboard</h1>
-	<div class="flex flex-wrap gap-2 max-w-md mx-auto pb-5" id="tagFilter">
+	<div class="max-w-md mx-auto pb-5" id="tagFilter">
 		<Svelecte
 			valueField="id"
 			labelField="name"
@@ -104,15 +105,16 @@
 			placeholder="Filter by tag..."
 			clearable="true"
 			class="tagFilter svelecte-control"
+			style="--sv-bg: rgb(51 65 85)"
 		/>
 	</div>
 	<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-2 max-w-7xl gap-x-5 mx-auto">
 		{#each memes as meme}
-			<div class="my-3 transition-all opacity-1 drop-shadow-md">
+			<div class="mb-4 transition-all drop-shadow-md rounded-xl">
 				<Lightbox transitionDuration={100}>
 					<img
 						slot="thumbnail"
-						class="rounded-t-sm !h-60 w-full object-cover"
+						class="!h-60 w-full object-cover"
 						src="https://xyquadrat.ch/artarindo/media/thumb/{correctFilename(meme.filename)}"
 						alt="Meme Thumbnail"
 					/>
@@ -126,50 +128,32 @@
 						/>
 					{/if}
 				</Lightbox>
-				<div class="rounded-b-sm dark:bg-slate-700 px-4 pt-2 h-14 flex items-center">
-					<a href={meme.message_url} target="_blank" rel="noreferrer">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-5 w-5 mr-2"
-							viewBox="0 0 20 20"
-							fill="currentColor"
+				<div class="rounded-b-xl bg-slate-700 px-4 py-2 flex flex-col">
+					<div class="flex">
+						<a
+							href={meme.message_url}
+							target="_blank"
+							rel="noreferrer"
+							class="flex items-center font-semibold"
 						>
-							<path
-								fill-rule="evenodd"
-								d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-					</a>
-					{meme.author_name}
-					<span class="ml-auto text-lg font-semibold text-red-500 flex items-center">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="inline"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							stroke-width="2"
-							stroke="currentColor"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						>
-							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-							<polyline points="6 15 12 9 18 15" />
-						</svg>
-						{meme.score}</span
-					>
-				</div>
-				<div class="text-black">
-					<Tags
-						on:tags={() => handleNewTag(meme.tags, meme.id)}
-						tags={meme.tags}
-						autoComplete={tags}
-						autoCompleteKey={'name'}
-						onlyAutocomplete={true}
-						onlyUnique={true}
-					/>
+							<Link class="h-4 w-4 mr-2" />
+							{meme.author_name}
+						</a>
+						<span class="ml-auto text-lg font-semibold text-sky-500 flex items-center">
+							<ChevronUp />
+							{meme.score}
+						</span>
+					</div>
+					<div class="text-black py-2 z-10">
+						<Tags
+							on:tags={() => handleNewTag(meme.tags, meme.id)}
+							tags={meme.tags}
+							autoComplete={tags}
+							autoCompleteKey={'name'}
+							onlyAutocomplete={true}
+							onlyUnique={true}
+						/>
+					</div>
 				</div>
 			</div>
 		{/each}
@@ -189,5 +173,9 @@
 		--sv-dropdown-shadow: 0 6px 12px rgba(243, 244, 246, 0.175);
 		--sv-highlight-bg: transparent;
 		--sv-highlight-color: rgb(51, 65, 85);
+	}
+	:global(.svelte-lightbox-thumbnail) {
+		overflow: hidden;
+		@apply rounded-t-xl;
 	}
 </style>
--- src/routes/tag/+page.svelte
+++ src/routes/tag/+page.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from 'svelte';
 	import Tags from 'svelte-tags-input';
 	import { shortcut } from '../../shortcut';
-	import { ChevronRight } from 'lucide-svelte';
+	import { ChevronRight, ChevronUp, Link } from 'lucide-svelte';
 
 	interface Meme {
 		id: number;
@@ -61,65 +61,49 @@
 	<div class="grid lg:grid-cols-3 mx-auto justify-center">
 		<div />
 		{#if meme}
-			<div class="mx-auto">
-				<div class="mb-5">
-					{#if isVideo}
-						<!-- svelte-ignore a11y-media-has-caption -->
-						<video controls src="https://xyquadrat.ch/artarindo/media/{meme.filename}" />
-					{:else}
-						<img src="https://xyquadrat.ch/artarindo/media/{meme.filename}" alt="Meme" />
-					{/if}
-				</div>
-				<div class="flex p-4">
-					<a href={meme.message_url} target="_blank" rel="noreferrer">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-5 w-5 mr-2"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-					</a>
-					<p>{meme.author_name}</p>
-					<span class="ml-auto text-lg font-semibold text-red-500">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="inline"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							stroke-width="2"
-							stroke="currentColor"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						>
-							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-							<polyline points="6 15 12 9 18 15" />
-						</svg>
-						{meme.score}</span
-					>
-				</div>
-				<div class="text-black">
-					<Tags
-						on:tags={handleNewTag}
-						tags={meme.tags}
-						autoComplete={tags}
-						autoCompleteKey={'name'}
-						onlyAutocomplete={true}
-						onlyUnique={true}
+			<div class="mb-4 drop-shadow-md rounded-xl overflow-hidden">
+				{#if isVideo}
+					<!-- svelte-ignore a11y-media-has-caption -->
+					<video controls src="https://xyquadrat.ch/artarindo/media/{meme.filename}" />
+				{:else}
+					<img
+						src="https://xyquadrat.ch/artarindo/media/{meme.filename}"
+						alt="Meme"
+						class="w-full object-cover"
 					/>
+				{/if}
+				<div class="rounded-b-xl bg-slate-700 px-4 py-2 flex flex-col">
+					<div class="flex">
+						<a
+							href={meme.message_url}
+							target="_blank"
+							rel="noreferrer"
+							class="flex items-center font-semibold"
+						>
+							<Link class="h-4 w-4 mr-2" />
+							{meme.author_name}
+						</a>
+						<span class="ml-auto text-lg font-semibold text-sky-500 flex items-center">
+							<ChevronUp />
+							{meme.score}
+						</span>
+					</div>
+					<div class="text-black py-2">
+						<Tags
+							on:tags={handleNewTag}
+							tags={meme.tags}
+							autoComplete={tags}
+							autoCompleteKey={'name'}
+							onlyAutocomplete={true}
+							onlyUnique={true}
+						/>
+					</div>
 				</div>
 			</div>
 		{/if}
 		<div class="mx-5 my-auto">
 			<button use:shortcut={{ code: 'ArrowRight' }} on:click={next}>
-				<ChevronRight />
+				<ChevronRight color="white" />
 			</button>
 		</div>
 	</div>
